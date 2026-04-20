9191d8341364329b1da175bcf53b10b1687d0b61
Will Jones <42045295+texasbones11@users.noreply.github.com>
2026-04-20 18:20:04 -0500

    Update script.js

--- docs/mistral-large-3/assets/script.js
+++ docs/mistral-large-3/assets/script.js
@@ -1,8 +1,12 @@
 import { createStore } from './store/store.js';
 import { renderApp } from './app.js';
+import { fetchLeaderboard } from './features/Leaderboard.js';
 
 // Initialize store
 const store = createStore();
+
+// Fetch leaderboard data
+fetchLeaderboard(store);
 
 // Render app
 renderApp(store);
